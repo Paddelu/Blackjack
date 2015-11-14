5cf42a82765525fc2402c5d637c6de2740011dcc
Paddelu <patrik.paarnio@gmail.com>
2015-11-14 14:20:39 +0200

    Translated to english

--- controller.js
+++ controller.js
@@ -1,21 +1,21 @@
 var player;
 var AI;
-var tulos;
+var score;
 var AIturn;
                         
 function initiate() {
     player = {score: 0};
     AI = {score: 0};
     AIturn = false;
-    document.getElementById("kortti").innerHTML = "Arvo kortti";
-    document.getElementById("Tulos").innerHTML = "Your score: " + player.score;
+    document.getElementById("card").innerHTML = "Deal a card";
+    document.getElementById("Score").innerHTML = "Your score: " + player.score;
     document.getElementById("AIt").innerHTML = "AI Waiting...";
     document.getElementById("AIs").innerHTML = "AI score: " + AI.score;
 }
 
 function Ai() {
     AIturn = true;
-    Kortti();
+    getCard();
 }
 
 function Winpopup() {
@@ -24,7 +24,7 @@ function Winpopup() {
         initiate();
     }
     else {
-        alert("Lähtee voittajana!");
+        alert("Leaves with a win!");
     }
 }
             
@@ -49,30 +49,33 @@ function Drawpopup() {
 }
 
 
-function logic(tulos) {
-    if (tulos > 21) {
+function logic(score) {
+    if (score > 21) {
         if (AIturn) {
             Winpopup();
         }
         else {
-        document.getElementById("Tulos").innerHTML = "Yli meni! Tuloksesi oli: "+ player.score;
+        document.getElementById("Score").innerHTML = "BUST! Better luck next time! Your score: "+ player.score;
         Losepopup();
         }
     }
-    else if (tulos == 21) {
+    else if (score == 21) {
         if (AIturn) {
+            document.getElementById("Score").innerHTML = "BLACKJACK! Better luck next time! Your score: "+ player.score;
             Losepopup();
         }
         else {
-            document.getElementById("Tulos").innerHTML = "Voitit! Tuloksesi oli tasan: "+ player.score;
+            document.getElementById("Score").innerHTML = "BLACKJACK! Your score: "+ player.score;
             Winpopup();
         } 
     }
     else if (AI.score >= 17 &&  AI.score == player.score){
+        document.getElementById("Score").innerHTML = "DRAW! Your score: "+ player.score;
         Drawpopup();
     }
     else if (AI.score >= 17 &&  AI.score > player.score) {
         if (AIturn) {
+            document.getElementById("Score").innerHTML = "Better luck next time! Your score: "+ player.score;
             Losepopup();
         }               
     }
@@ -88,43 +91,43 @@ function logic(tulos) {
     }
 }
             
-function laskin(arvo) {
+function laskin(value) {
     if (AIturn) {
-        AI.score = AI.score + arvo;
+        AI.score = AI.score + value;
         document.getElementById("AIs").innerHTML = "AI score: " + AI.score;
         logic(AI.score);
     }
     else {
-        player.score = player.score + arvo;
-        document.getElementById("Tulos").innerHTML = "Your score: " + player.score;
+        player.score = player.score + value;
+        document.getElementById("Score").innerHTML = "Your score: " + player.score;
         logic(player.score);     
     }
 }
             
-function Kortti() {
-    var arvo, arvoN, maa, text = "Arpomani kortti on ";
+function getCard() {
+    var value, valueN, maa, text = "The card dealt is ";
     maa = Math.floor((Math.random() * 4) + 1);
     switch (maa) {
-        case 1: maa = "Hertta "; break;
-        case 2: maa = "Pata "; break;
-        case 3: maa = "Ruutu "; break;
-        case 4: maa = "Risti "; break;         
+        case 1: maa = "♥ "; break;
+        case 2: maa = "♠ "; break;
+        case 3: maa = "♦ "; break;
+        case 4: maa = "♣ "; break;         
     }
-    arvo = Math.floor((Math.random() * 13) + 1);
-    switch (arvo) {
-        case 1: arvoN ="Ässä"; break;
-        case 11: arvoN ="Jätkä"; break;
-        case 12: arvoN ="Rouva"; break;
-        case 13: arvoN ="Kuningas"; break;
-        default: arvoN = arvo; break;      
+    value = Math.floor((Math.random() * 13) + 1);
+    switch (value) {
+        case 1: valueN ="ace"; break;
+        case 11: valueN ="Jack"; break;
+        case 12: valueN ="Queen"; break;
+        case 13: valueN ="King"; break;
+        default: valueN = value; break;      
     }
     if(AIturn) {
-        document.getElementById("AIt").innerHTML = text + maa + arvoN;
-        laskin(arvo); 
+        document.getElementById("AIt").innerHTML = text + valueN + " of " + maa;
+        laskin(value); 
     }
     else {
-        document.getElementById("kortti").innerHTML = text + maa + arvoN;
-        laskin(arvo);  
+        document.getElementById("card").innerHTML = text + valueN + " of " + maa;
+        laskin(value);  
     }
 }
 initiate();
